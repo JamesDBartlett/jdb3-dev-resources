03ac87bfc7cd1069d306c718eacbc3564f7c4c99
James D. Bartlett III <jimmyb@gmail.com>
2018-08-16 00:43:14 -0500

    added some stuff

--- sh/mac/task_switch_reminder.sh
+++ sh/mac/task_switch_reminder.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
+
+# Set time length for each task segment in minutes
+l=30
+
+# Set time to allow for switching between tasks in minutes
+w=3
+
 i=0
 j=1
 x=0
-s=1800
-t=20
+s=$((l * 60))
+t=$((w * 60))
+
+say Initializing task switch reminder program. Task intervals are $l minutes, and breaks between tasks are $w minutes. Beginning first task. 
 while true; do
 clear
 until [ $i -gt $s ]; do
@@ -13,15 +22,16 @@ echo -ne $x
 echo -ne " seconds until task switch...\r"
 let i+=1
 done
-say Attention: It is time to switch tasks. You have $t seconds to find a new task!
+say Attention: It is time to switch tasks. You have $w minutes to save your work and find a new task!
+sleep 0.1
 clear
 let j=1
-until [ $j -gt $t ]; do
-say $j
+until [ $j -gt $w ]; do
+# say $j
 sleep 0.25
 let j+=1
 done
-say Begin your new task now!
+say Begin your new task now! You have $l minutes.
 let i=0
 let j=1
 done
